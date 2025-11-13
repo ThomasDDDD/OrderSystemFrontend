f277293bdd74f62f06ac6a407fbdff4215ba53d1
ThomasDDDD <thomasdtigt@googlemail.com>
2025-11-13 14:00:32 +0100

    fix: dies das

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -15,63 +15,63 @@ const BottomBar = () => {
       {isLoggedIn && (
         <div className="container mx-auto max-w-[1400px] ">
           <ul className="flex justify-between gap-2 p-2 lg:p-8 mx-auto ">
-            <li className="w-[25%]  md:w-[20%] text-center cursor-pointer px-2 py-1 border border-[var(--text-color)]">
-              <a
-                className="hover:underline  text-(length:--font-size-sub-nav) font-(family-name:--style-font) transition-all duration-500 ease-in-out hover:text-[var(--accent-color-100)]"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setStatus("pending");
-                }}
-              >
+            <li
+              className="w-[25%]  md:w-[20%] text-center cursor-pointer px-2 py-1 border border-[var(--text-color)]"
+              onClick={(e) => {
+                e.preventDefault();
+                setStatus("pending");
+              }}
+            >
+              <a className="hover:underline  text-(length:--font-size-sub-nav) font-(family-name:--style-font) transition-all duration-500 ease-in-out hover:text-[var(--accent-color-100)]">
                 Offen
               </a>
             </li>
-            <li className="w-[25%] md:w-[20%] text-center cursor-pointer px-2 py-1 border border-[var(--text-color)]">
-              <a
-                className="hover:underline  text-(length:--font-size-sub-nav) font-(family-name:--style-font) transition-all duration-500 ease-in-out hover:text-[var(--accent-color-100)]"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setStatus("completed");
-                }}
-              >
+            <li
+              className="w-[25%] md:w-[20%] text-center cursor-pointer px-2 py-1 border border-[var(--text-color)]"
+              onClick={(e) => {
+                e.preventDefault();
+                setStatus("completed");
+              }}
+            >
+              <a className="hover:underline  text-(length:--font-size-sub-nav) font-(family-name:--style-font) transition-all duration-500 ease-in-out hover:text-[var(--accent-color-100)]">
                 Fertig
               </a>
             </li>
 
-            <li className="md:w-[20%] text-center border-[var(--text-color)] relative  ">
-              <a
-                className=" hover:underline cursor-pointer text-(length:--font-size-XXXL) font-(family-name:--style-font) absolute translate-y-[-120%] md:translate-y-[-60%] translate-x-[-50%] hover:scale-[120%] transition-all duration-500 ease-in-out "
-                onClick={(e) => {
-                  e.preventDefault();
+            <li
+              className="md:w-[20%] text-center border-[var(--text-color)] relative  "
+              onClick={(e) => {
+                e.preventDefault();
 
-                  setNewOrderPopUp(!newOrderPopUp);
-                }}
-              >
+                setNewOrderPopUp(!newOrderPopUp);
+              }}
+            >
+              <a className=" hover:underline cursor-pointer text-(length:--font-size-XXXL) font-(family-name:--style-font) absolute translate-y-[-120%] md:translate-y-[-60%] translate-x-[-50%] hover:scale-[120%] transition-all duration-500 ease-in-out ">
                 <div className="bg-[var(--accent-color-50)]">
                   <FaPlusSquare className="scale-[120%]  text-[var(--primary-color-100)]" />
                 </div>
               </a>
             </li>
-            <li className="w-[25%]  md:w-[20%] text-center cursor-pointer px-2 py-1 border border-[var(--text-color)]">
-              <a
-                className="hover:underline  text-(length:--font-size-sub-nav) font-(family-name:--style-font) transition-all duration-500 ease-in-out hover:text-[var(--accent-color-100)]"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setStatus("");
-                }}
-              >
+            <li
+              className="w-[25%]  md:w-[20%] text-center cursor-pointer px-2 py-1 border border-[var(--text-color)]"
+              onClick={(e) => {
+                e.preventDefault();
+                setStatus("");
+              }}
+            >
+              <a className="hover:underline  text-(length:--font-size-sub-nav) font-(family-name:--style-font) transition-all duration-500 ease-in-out hover:text-[var(--accent-color-100)]">
                 Alle
               </a>
             </li>
 
-            <li className="w-[25%]  md:w-[20%] text-center cursor-pointer px-2 py-1 border border-[var(--text-color)]">
-              <a
-                className="hover:underline text-(length:--font-size-sub-nav) font-(family-name:--style-font) transition-all duration-500 ease-in-out hover:text-[var(--accent-color-100)]"
-                onClick={(e) => {
-                  e.preventDefault();
-                  console.log("irgendwas auslösen 4");
-                }}
-              >
+            <li
+              className="w-[25%]  md:w-[20%] text-center cursor-pointer px-2 py-1 border border-[var(--text-color)]"
+              onClick={(e) => {
+                e.preventDefault();
+                console.log("irgendwas auslösen 4");
+              }}
+            >
+              <a className="hover:underline text-(length:--font-size-sub-nav) font-(family-name:--style-font) transition-all duration-500 ease-in-out hover:text-[var(--accent-color-100)]">
                 Button 4
               </a>
             </li>
